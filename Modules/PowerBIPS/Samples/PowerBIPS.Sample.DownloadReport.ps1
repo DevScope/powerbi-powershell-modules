c2b273b1718022a37361559b8c262c071775d8e9
Rui Romano <ruiromano@outlook.com>
2017-09-28 18:32:46 +0100

    version

--- Modules/PowerBIPS/Samples/PowerBIPS.Sample.DownloadReport.ps1
+++ Modules/PowerBIPS/Samples/PowerBIPS.Sample.DownloadReport.ps1
@@ -6,7 +6,7 @@ $currentPath = (Split-Path $MyInvocation.MyCommand.Definition -Parent)
 
 Import-Module "$currentPath\..\PowerBIPS" -Force
 
-$authToken = Get-PBIAuthToken # -ForceAskCredentials -Verbose
+$authToken = Get-PBIAuthToken 
 
 Set-PBIGroup -authToken $authToken -name "Demos - PBIFromTrenches" -Verbose
 
