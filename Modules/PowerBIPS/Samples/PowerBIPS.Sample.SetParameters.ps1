edaf3b09675ff104e78b520b5673cf343c846b2b
Fábio Miguel Moreira Batista <fabiommb@hotmail.com>
2018-03-01 18:38:21 +0000

    New cmdlets: Get-PBIDatasetParameters and Set-PBIDatasetParameters

---
+++ Modules/PowerBIPS/Samples/PowerBIPS.Sample.SetParameters.ps1
@@ -0,0 +1,23 @@
+﻿cls
+
+$ErrorActionPreference = "Stop"
+
+$currentPath = (Split-Path $MyInvocation.MyCommand.Definition -Parent)
+
+Import-Module "$currentPath\..\PowerBIPS" -Force
+
+$authToken = Get-PBIAuthToken -Verbose
+
+#Set-PBIGroup -authToken $authToken -id "someWorkspaceID"
+
+$parameters = @(
+    @{
+        name="SomeParameter"
+        newValue="SomeValue"
+    }
+)
+
+Set-PBIDatasetParameters -authToken $authToken -parameters $parameters -datasetNames "SomeDataset"
+
+$updatedParameters = Get-PBIDatasetParameters -authToken $authToken -datasetNames "SomeDataset"
+$updatedParameters | Format-Table
